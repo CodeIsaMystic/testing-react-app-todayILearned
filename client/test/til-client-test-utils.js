841626079540974f0ab2b450dfa7a44447d9268c
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-21 15:27:55 +0100

    npm depedencies for client, server & shared folders project too

--- client/test/til-client-test-utils.js
+++ client/test/til-client-test-utils.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Router} from 'react-router-dom'
-import {render, wait} from 'react-testing-library'
-import {createMemoryHistory} from 'history'
+import { Router } from 'react-router-dom'
+import { render, wait } from 'react-testing-library'
+import { createMemoryHistory } from 'history'
 import 'jest-dom/extend-expect'
 import * as generate from 'til-shared/generate'
 
-function renderWithRouter(ui, {route = '/', ...renderOptions} = {}) {
-  const history = createMemoryHistory({initialEntries: [route]})
+function renderWithRouter(ui, { route = '/', ...renderOptions } = {}) {
+  const history = createMemoryHistory({ initialEntries: [route] })
   const utils = render(<Router history={history}>{ui}</Router>, renderOptions)
   const finishLoading = () =>
     wait(() => expect(utils.queryByText('Loading')).toBeNull())
@@ -25,4 +25,4 @@ export {
   renderIntoDocument,
   fireEvent,
 } from 'react-testing-library'
-export {renderWithRouter, generate}
+export { renderWithRouter, generate }
